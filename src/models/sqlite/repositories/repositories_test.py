f99c0bc4c9a3401c120cf2a1f20236613d557798
devGabyAlves <gabriellyalves401@gmail.com>
2024-09-28 13:04:56 -0300

    feat: Implementation of the logic to delete a pet based on its name using SQLAlchemy.

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -9,3 +9,8 @@ def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
     print(response)
+
+def test_delete_pet():
+    name = "belinha"
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
