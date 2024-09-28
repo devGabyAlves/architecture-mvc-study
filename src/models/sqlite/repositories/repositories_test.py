09b59bb416757cdfe57e48b6176740360e6a484e
devGabyAlves <gabriellyalves401@gmail.com>
2024-09-28 12:54:46 -0300

    feat: Implementation of special methods and integration with database

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,11 @@
+import pytest
+from src.models.sqlite.settings import db_connection_handler
+from .pets_repository import PetsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Interaction with the bank")
+def test_list_pets():
+    repo = PetsRepository(db_connection_handler)
+    response = repo.list_pets()
+    print(response)
